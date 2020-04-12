27bb8a723d5a0c1b41bfbdf18b73eb3b6926da8d
SAIDIAEK <a_saidi@esi.dz>
2020-04-12 21:20:29 +0100

    paypal button deletedd

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 //import PayPalButton from './PayPalButton'
 
-export default function CartTotals({value, history}) {
+export default function CartTotals({value}) {
     const {cartSubTotal, cartTax, cartTotal, clearCart} = value;
     return (
         <React.Fragment>
