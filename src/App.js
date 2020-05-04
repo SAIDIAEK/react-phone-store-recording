e0e23599235d126ad88d91cce9b86ae385b04832
SAIDIAEK <63463509+SAIDIAEK@users.noreply.github.com>
2020-05-04 14:16:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
-//import Navbar from './components/Navbar'
+import Navbar from './components/Navbar'
 import ProductList from './components/ProductList'
 import Details from './components/Details'
 import Cart from './components/Cart'
 import Default from './components/Default'
 import Modal from './components/Modal'
-import { NavigationBar } from './components/NavigationBar';
-import Sidebar from './components/Sidebar'
+//import { NavigationBar } from './components/NavigationBar';
+//import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <React.Fragment>
-      <NavigationBar />
+    {/* <NavigationBar /> */}
 
     {/* <Sidebar /> */}
-      {/* <Navbar /> */}
+       <Navbar />
        <Switch>
         <Route exact path="/" component={ProductList} />
         <Route exact path="/phone" component={ProductList} categorie="phone"/>
